66880190605411497d2b1b9e7f2734ec74fd4486
developerworks <developerworks@163.com>
2017-12-08 22:09:27 +0800

    Add api annotations for data rest

--- src/main/java/com/example/examplespringdatarest/repository/UserRepository.java
+++ src/main/java/com/example/examplespringdatarest/repository/UserRepository.java
@@ -1,23 +1,23 @@
 /**
  * RequestMapping 不能注解在 Repository 上面.Repository 方法不能修改返回结果的格式(默认为JSON).
  * 如果需要服务返回XML格式的, 需要一个控制器来调用 Repository
+ * <p>
+ * https://reflectoring.io/documenting-spring-data-rest-api-with-springfox/
  */
-
 package com.example.examplespringdatarest.repository;
 
 import com.example.examplespringdatarest.entity.User;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.web.bind.annotation.RequestParam;
 
-@Api(value = "API - UserRepository", description = "用户信息管理")
-@RepositoryRestResource(path = "user")
+@Api(tags = "用户对象管理")
+@RepositoryRestResource(path = "users")
 public interface UserRepository extends JpaRepository<User, Long> {
 
     /**
@@ -28,7 +28,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
      * @return
      */
     @RestResource(path = "nameStartsWith", rel = "nameStartsWith")
-    @ApiParam(name = "username", value = "用户名称", required = true)
+    @ApiOperation("查找以字符串开头的用户名称")
     Page<User> findByUsernameStartsWith(@Param("username") String username, Pageable pageable);
 
     /**
